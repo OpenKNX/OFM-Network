4040bfb15b4d19894ecb99978409ddaf140fa316
Ing-Dom <dom@ing-dom.de>
2023-05-24 16:26:24 +0200

    wip

--- src/IPConfig.h
+++ src/IPConfig.h
@@ -13,6 +13,9 @@ class IPConfigModule : public OpenKNX::Module
 	private:
         uint8_t *_data;
         IPAddress GetIpProperty(uint8_t PropertyId);
+        void SetIpProperty(uint8_t PropertyId, IPAddress IPAddress);
+        uint8_t GetByteProperty(uint8_t PropertyId);
+        void SetByteProperty(uint8_t PropertyId, uint8_t date);
 };
 
 //Give your Module a name
@@ -32,43 +35,62 @@ const std::string IPConfigModule::version()
 
 void IPConfigModule::init()
 {
-    logInfoP("IPConfigModule::init");
+    logInfoP("Init IP Stack");
 
-    byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x01};
+    uint32_t serial = knx.platform().uniqueSerialNumber();
+    uint8_t serialBytes[4];
+    pushInt(knx.platform().uniqueSerialNumber(), serialBytes);
+    byte mac[] = {0x60, 0x4A, 0x7B, serialBytes[1], serialBytes[2], serialBytes[3]};
+
+    logHexTraceP("MAC-Address: ", mac, 6);
 
     randomSeed(millis());
 
-    // Setup the GPIOs TODO: SPI / SPI1 selection.
-    pinMode(PIN_SS_, OUTPUT);
-    digitalWrite(PIN_SS_, HIGH);
-    SPI1.setRX(PIN_MISO_);
-    SPI1.setTX(PIN_MOSI_);
-    SPI1.setSCK(PIN_SCK_);
-    SPI1.setCS(PIN_SS_);
-    //SPI.setRX(PIN_MISO_);
-    //SPI.setTX(PIN_MOSI_);
-    //SPI.setSCK(PIN_SCK_);
-    //SPI.setCS(PIN_SS_);
+    SPIClassRP2040 *spi;
+    if(USING_SPI2)
+    {
+        spi = &SPI1;
+        logTraceP("Using SPI1 for Ethernet");
+    }
+    else
+    {
+        spi = &SPI;
+        logTraceP("Using SPI for Ethernet");
+    }
 
-    logInfoP("Setup Pins done ....");
+    pinMode(PIN_SS_, OUTPUT);
+    digitalWrite(PIN_SS_, HIGH); // Todo check if this is neccessary
+    spi->setRX(PIN_MISO_);
+    spi->setTX(PIN_MOSI_);
+    spi->setSCK(PIN_SCK_);
+    spi->setCS(PIN_SS_);
+
+    logTraceP("Ethernet SPI GPIO: RX/MISO: %d, TX/MOSI: %d, SCK/SCLK: %d, CSn/SS: %d", PIN_MISO_, PIN_MOSI_, PIN_SCK_, PIN_SS_);
 
     Ethernet.init(PIN_SS_);
-    logInfoP("Initialized ");
 
-    // uint8_t NoOfElem = 30;
-    // uint8_t *FriendlyName;
-    // uint32_t length;
-    // knx.bau().propertyValueRead(OT_IP_PARAMETER, 0, PID_FRIENDLY_NAME, NoOfElem, 1, &FriendlyName, length);
-    // wenn leer dann default-wert ( oder doch nur wenn "entladen")
-    // Ethernet.setHostname()
+    if(knx.configured())
+    {
+        uint8_t NoOfElem = 30;
+        uint8_t *FriendlyName;
+        uint32_t length;
+        knx.bau().propertyValueRead(OT_IP_PARAMETER, 0, PID_FRIENDLY_NAME, NoOfElem, 1, &FriendlyName, length);
+        Ethernet.setHostname((const char *)FriendlyName);
+        delete[] FriendlyName;
+    }
+    else
+    {
+        Ethernet.setHostname(MAIN_OrderNumber);
+    }
 
-    uint8_t NoOfElem = 1;
-    uint8_t *data;
-    uint32_t length;
-    knx.bau().propertyValueRead(OT_IP_PARAMETER, 0, PID_IP_ASSIGNMENT_METHOD, NoOfElem, 1, &data, length);
+    //uint8_t NoOfElem = 1;
+    //uint8_t *data;
+    //uint32_t length;
+    //knx.bau().propertyValueRead(OT_IP_PARAMETER, 0, PID_IP_ASSIGNMENT_METHOD, NoOfElem, 1, &data, length);
     
     uint8_t EthernetState = 1;
-    switch(*data)
+    //switch(*data)
+    switch(GetByteProperty(PID_IP_ASSIGNMENT_METHOD))
     {
         case 1: // manually see 2.5.6 of 03_08_03
         {
@@ -122,4 +144,39 @@ IPAddress IPConfigModule::GetIpProperty(uint8_t PropertyId)
     IPAddress ret = (data[3] << 24) + (data[2] << 16) + (data[1] << 8) + data[0];
     delete[] data;
     return ret;
+}
+
+void IPConfigModule::SetIpProperty(uint8_t PropertyId, IPAddress IPAddress)
+{
+    uint8_t NoOfElem = 1;
+    uint8_t data[4];
+
+    data[0] = IPAddress[0];
+    data[1] = IPAddress[1];
+    data[2] = IPAddress[2];
+    data[3] = IPAddress[3];
+
+    knx.bau().propertyValueWrite(OT_IP_PARAMETER, 0, PropertyId, NoOfElem, 1, data, 0);
+}
+
+uint8_t IPConfigModule::GetByteProperty(uint8_t PropertyId)
+{
+    uint8_t NoOfElem = 1;
+    uint8_t *data;
+    uint8_t ret;
+    uint32_t length;
+    knx.bau().propertyValueRead(OT_IP_PARAMETER, 0, PropertyId, NoOfElem, 1, &data, length);
+    ret = data[0];
+    delete[] data;
+    return ret;
+}
+
+void IPConfigModule::SetByteProperty(uint8_t PropertyId, uint8_t value)
+{
+    uint8_t NoOfElem = 1;
+    uint8_t data[1];
+
+    data[0] = value;
+
+    knx.bau().propertyValueWrite(OT_IP_PARAMETER, 0, PropertyId, NoOfElem, 1, data, 0);
 }
