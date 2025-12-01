7d96a130febbe240a28460a892a6119789835dcb
Dom <dom@ing-dom.de>
2025-12-01 21:58:05 +0100

    Adapt to StatusLED Feature in Common 1.6 (#25)
    Adapt to StatusLED Feature in Common 1.6
    
    
    ---------
    
    Co-authored-by: Waldemar Porscha <wp@porscha.eu>

--- src/NetworkModule.cpp
+++ src/NetworkModule.cpp
@@ -311,6 +311,8 @@ void NetworkModule::setup(bool configured)
 
     registerCallback([this](bool state) { if (state) this->showNetworkInformations(false); });
 
+    _ipLedFunc = openknx.ledFunctions.get(OPENKNX_LEDFUNC_NET_STATE);
+
     if (!configured || ParamNET_mDNS)
     {
         logDebugP("Start mDNS");
@@ -430,16 +432,14 @@ void NetworkModule::checkLinkStatus()
         newLinkState = true;
     else
         newLinkState = connected();
-#ifdef OPENKNX_LED_IP
-    // update LED's
+    
     if (newLinkState && established())
     {
         if (_ipLedState != 1)
         {
-#ifdef OPENKNX_SERIALLED_ENABLE
-            openknx.OPENKNX_LED_IP.setColor(OPENKNX_SERIALLED_COLOR_GREEN);
-#endif
-            openknx.OPENKNX_LED_IP.on();
+            _ipLedFunc->setColor(OpenKNX::Led::Color::Green);
+            _ipLedFunc->activity(OpenKNX::Led::g_ipLedActivity, true);
+
             _ipLedState = 1;
         }
     }
@@ -447,12 +447,9 @@ void NetworkModule::checkLinkStatus()
     {
         if (_ipLedState != 2)
         {
-#ifdef OPENKNX_SERIALLED_ENABLE
-            openknx.OPENKNX_LED_IP.setColor(OPENKNX_SERIALLED_COLOR_YELLOW);
-            openknx.OPENKNX_LED_IP.on();
-#else
-            openknx.OPENKNX_LED_IP.blinking(1000);
-#endif
+            _ipLedFunc->setColor(OpenKNX::Led::Color::Yellow);
+            _ipLedFunc->on(OpenKNX::Led::Capability::COLOR);
+            _ipLedFunc->blinking(1000, OpenKNX::Led::Capability::MONOCHROME);
 
             _ipLedState = 2;
         }
@@ -464,29 +461,23 @@ void NetworkModule::checkLinkStatus()
         {
             if (_ipLedState != 4)
             {
-#ifdef OPENKNX_SERIALLED_ENABLE
-                openknx.OPENKNX_LED_IP.setColor(OPENKNX_SERIALLED_COLOR_RED);
-                openknx.OPENKNX_LED_IP.blinking(500);
-#else
-                openknx.OPENKNX_LED_IP.blinking(500);
-#endif
+                _ipLedFunc->setColor(OpenKNX::Led::Color::Red);
+                _ipLedFunc->blinking(500);
                 _ipLedState = 4;
             }
         }
         else
 #endif
-            if (_ipLedState != 3)
+        if (_ipLedState != 3)
         {
-#ifdef OPENKNX_SERIALLED_ENABLE
-            openknx.OPENKNX_LED_IP.setColor(OPENKNX_SERIALLED_COLOR_RED);
-            openknx.OPENKNX_LED_IP.on();
-#else
-            openknx.OPENKNX_LED_IP.off();
-#endif
+
+            _ipLedFunc->setColor(OpenKNX::Led::Color::Red);
+            _ipLedFunc->on(OpenKNX::Led::Capability::COLOR);
+            _ipLedFunc->off(OpenKNX::Led::Capability::MONOCHROME);
             _ipLedState = 3;
         }
     }
-#endif
+
     // got link
     if (newLinkState && !_currentLinkState)
     {
